819975c08cb7e25c9e737b1539184c37634c108b
Nikita Shevkunov <shevkunovnikita97@gmail.com>
2021-09-15 07:04:53 +0500

    connecting to a database, creating schemas and models

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TrackModule } from './tracks/track.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 
 @Module({
-  imports: [TrackModule]
+  imports: [
+    MongooseModule.forRoot('mongodb+srv://nikita_admin:4dFgbOI8YY3I4dh8@cluster0.sjxbp.mongodb.net/music-platform?retryWrites=true&w=majority'),
+    TrackModule
+  ]
 })
 
 export class AppModule {}
