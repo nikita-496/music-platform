011247873486fa2e28f1a0603297fcc8e2c91df4
Nikita Shevkunov <shevkunovnikita97@gmail.com>
2021-09-15 07:09:49 +0500

    access to models, using them inside services

--- server/src/tracks/track.module.ts
+++ server/src/tracks/track.module.ts
@@ -1,9 +1,18 @@
+import { CommentSchema } from './schemes/comments.schema';
+import { MongooseModule } from '@nestjs/mongoose';
 import { Module } from "@nestjs/common";
 import { TrackController } from "./track.controller";
 import { TrackService } from "./track.service";
+import { Track, TrackSchema } from './schemes/track.schema';
 
 @Module({
   //регистрация в модуле 
+
+  //доступ к моделям для сервисов и контроллеров
+  imports: [
+    MongooseModule.forFeature( [{name: Track.name, schema:TrackSchema}]),
+    MongooseModule.forFeature( [{name: Comment.name, schema:CommentSchema}]),
+  ],
   controllers: [TrackController],
   providers:[TrackService]
 
--- server/src/tracks/track.service.ts
+++ server/src/tracks/track.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from "@nestjs/common";
-
-
+import { InjectModel } from "@nestjs/mongoose";
+import { Model } from "mongoose";
+import { Comment } from "./schemes/comments.schema";
+import { Track } from "./schemes/track.schema";
 @Injectable()
 
 export class TrackService{
+
+  
+  //использование модели внутри сервиса
+  constructor(@InjectModel(Track.name) private trackModel: Model<TrackDocument>,
+              @InjectModel(Comment.name) private commentkModel: Model<CommentDocument>){}
+
    //создание трека 
   async create() {
 
